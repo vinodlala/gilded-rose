01c7efb3714fadda14ac441c479b257e5bb84459
Vinod Lala <vinod_lala@usa.net>
2019-03-17 12:22:15 -0400

    Clean up, rename hash, add comments, re-order methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,7 +1,7 @@
 class GildedRose
   attr_reader :name, :days_remaining, :quality
 
-  METHODS_HASH = {
+  SPECIAL_ITEM_METHODS = {
     "Aged Brie": "tick_aged_brie",
     "Backstage passes to a TAFKAL80ETC concert": "tick_concert",
     "Conjured Mana Cake": "tick_cake",
@@ -18,115 +18,14 @@ class GildedRose
     @quality = quality
   end
 
-  def tick_old
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
-  end
-
-  def tick_not_as_old
-    method_name = METHODS_HASH[@name.to_sym]
-    if method_name
-      send(method_name)
-      puts "hi"
-      return
-    end
-
-    # Aged Brie
-    if is_aged_brie?
-      tick_aged_brie
-      return
-    end
-
-    # Sulfuras, Hand of Ragnaros
-    if is_sulfurus?
-      return
-    end
-
-    # Backstage passes to a TAFKAL80ETC concert
-    if is_concert?
-      tick_concert
-      return
-    end
-
-    # Conjured Mana Cake
-    if is_cake?
-      # decrease_days_remaining
-      #
-      # if negative_days_remaining?
-      #   decrease_quality(4)
-      # else
-      #   decrease_quality(2)
-      # end
-      tick_cake
-
-      return
-    end
-
-    # Normal Item
-    decrease_quality(1)
-    decrease_days_remaining
-    if negative_days_remaining?
-      decrease_quality(1)
-    end
-  end
-
   def tick
-    method_name = METHODS_HASH[@name.to_sym]
+    method_name = SPECIAL_ITEM_METHODS[@name.to_sym]
 
     if method_name
       send(method_name)
-      return
+    else
+      tick_normal_item
     end
-
-    tick_normal_item
-    # decrease_quality(1)
-    #
-    # decrease_days_remaining
-    #
-    # if negative_days_remaining?
-    #   decrease_quality(1)
-    # end
   end
 
   def tick_aged_brie
@@ -153,23 +52,23 @@ class GildedRose
     decrease_days_remaining
 
     case days_remaining
-    when 10..999
+    when 10..Float::INFINITY
       increase_quality(1)
     when 5..9
       increase_quality(2)
     when 0..4
       increase_quality(3)
     else
-      decrease_quality(@quality)
+      make_quality_zero
     end
   end
 
   def tick_normal_item
-    decrease_quality(1)
-
     decrease_days_remaining
 
     if negative_days_remaining?
+      decrease_quality(2)
+    else
       decrease_quality(1)
     end
   end
@@ -177,39 +76,29 @@ class GildedRose
   def tick_sulfurus
   end
 
-  def decrease_quality(change)
-    changed_quality = @quality - change
-    # @quality = [changed_quality, 0].max
-    @quality = [changed_quality, MIN_QUALITY].max
-  end
-
   def decrease_days_remaining
     @days_remaining = @days_remaining - 1
   end
 
+  def decrease_quality(change)
+    changed_quality = @quality - change
+
+    # Prevent quality from going below MIN_QUALITY of 0
+    @quality = [changed_quality, MIN_QUALITY].max
+  end
+
   def increase_quality(change)
     changed_quality = @quality + change
-    # @quality = [changed_quality, 50].min
+
+    # Prevent quality from going above MAX_QUALITY of 50
     @quality = [changed_quality, MAX_QUALITY].min
+  end
+
+  def make_quality_zero
+    @quality = 0
   end
 
   def negative_days_remaining?
     @days_remaining < 0
-  end
-
-  def is_aged_brie?
-    @name == "Aged Brie"
-  end
-
-  def is_concert?
-    @name == "Backstage passes to a TAFKAL80ETC concert"
-  end
-
-  def is_cake?
-    @name == "Conjured Mana Cake"
-  end
-
-  def is_sulfurus?
-    @name == "Sulfuras, Hand of Ragnaros"
   end
 end
